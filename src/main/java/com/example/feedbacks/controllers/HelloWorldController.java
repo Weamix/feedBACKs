0364cdf555f7c01210cb941258f5c858634e6185
Vitse Maxime <maxime.vitse@decathlon.com>
2022-03-24 14:02:57 +0100

    /test controller working

---
+++ src/main/java/com/example/feedbacks/controllers/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.feedbacks.controllers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HelloWorldController {
+
+    @GetMapping
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
