c69434e2f60d9a7662a7fc0c8d6570dd486f1c8d
Vitse Maxime <maxime.vitse@decathlon.com>
2022-03-24 17:42:33 +0100

    Add form

---
+++ src/main/java/com/example/feedbacks/entities/Form.java
@@ -0,0 +1,11 @@
+package com.example.feedbacks.entities;
+
+import java.util.List;
+
+public class Form {
+    private Integer id;
+    private String author;
+    private List<Question> questions;
+
+    // TODO: add getters and setters.
+}
