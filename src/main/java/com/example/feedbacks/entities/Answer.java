0200d97f79eb2694a9fda9c5a152f7d1af742d39
Axel Lebas <lebas.axel@gmail.com>
2022-05-03 15:42:31 +0200

    switching int to Integer for id type of Answer Entity

--- src/main/java/com/example/feedbacks/entities/Answer.java
+++ src/main/java/com/example/feedbacks/entities/Answer.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Answer {
-    private int id;
+    private Integer id;
     private String message;
     // stocké l'auteur car une question peut avoir une réponse par plusieurs personnes
     private String author;
