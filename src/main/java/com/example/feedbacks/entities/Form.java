3372aa80bd33515f399d97ed42fcc2a68f020c28
Vitse Maxime <maxime.vitse@decathlon.com>
2022-05-11 22:35:21 +0200

    JPA Hibernate

--- src/main/java/com/example/feedbacks/entities/Form.java
+++ src/main/java/com/example/feedbacks/entities/Form.java
@@ -3,6 +3,7 @@ package com.example.feedbacks.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,10 +11,28 @@ import java.util.Set;
 
 @Getter
 @Setter
+@Entity
 public class Form {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String author;
 
+    @ElementCollection
+    @OneToMany
     private Set<Question> questions;
-    private HashMap<Integer, ArrayList<Answer>> answers;
+
+    /*@OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "answer_to_question_mapping", joinColumns = {
+            @JoinColumn(name = "question_id", referencedColumnName = "id")
+    }, inverseJoinColumns = {
+            @JoinColumn(name = "answer_id", referencedColumnName = "id")
+    })
+    @MapKey(name = "question_id")*/
+
+    @ElementCollection
+    @OneToMany(mappedBy = "question_id")
+    @MapKey(name = "question_id")
+    private Map<Integer, ArrayList<Answer>> answers;
 }
--- src/main/java/com/example/feedbacks/entities/Question.java
+++ src/main/java/com/example/feedbacks/entities/Question.java
@@ -2,9 +2,18 @@ package com.example.feedbacks.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
+@Entity
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String message;
 }
