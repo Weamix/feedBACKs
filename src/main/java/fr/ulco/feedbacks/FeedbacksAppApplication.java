de8077ef9413358339db375441b0d0513a90eb2b
Vitse Maxime <maxime.vitse@decathlon.com>
2022-05-26 16:46:13 +0200

    [API] SignUp,Login,Role

--- src/main/java/fr/ulco/feedbacks/FeedbacksAppApplication.java
+++ src/main/java/fr/ulco/feedbacks/FeedbacksAppApplication.java
@@ -4,8 +4,10 @@ import fr.ulco.feedbacks.dto.AnswerDto;
 import fr.ulco.feedbacks.dto.FormDto;
 import fr.ulco.feedbacks.dto.QuestionDto;
 import fr.ulco.feedbacks.entity.Role;
+import fr.ulco.feedbacks.entity.RoleName;
 import fr.ulco.feedbacks.entity.User;
 import fr.ulco.feedbacks.service.FormService;
+import fr.ulco.feedbacks.service.RoleService;
 import fr.ulco.feedbacks.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,11 +18,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class FeedbacksAppApplication {
-	private static final String ADMIN = "ADMIN";
-	private static final String USER = "USER";
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbacksAppApplication.class, args);
@@ -33,18 +34,18 @@ public class FeedbacksAppApplication {
 
 	//FAKE DATA FOR BDD
 	@Bean
-	CommandLineRunner run(UserService userService, FormService formService) {
+	CommandLineRunner run(UserService userService, FormService formService, RoleService roleService) {
 		return args -> {
-			userService.saveRole(new Role(null, USER));
-			userService.saveRole(new Role(null, ADMIN));
+			roleService.saveRole(new Role(null, RoleName.USER));
+			roleService.saveRole(new Role(null, RoleName.ADMIN));
 
-			userService.saveUser(new User(null, "weamix", "maxime", "maxime.vitse@decathlon.com", new ArrayList<>()));
-			userService.saveUser(new User(null, "alebas", "lebas", "axel.lebas@decathlon.com", new ArrayList<>()));
-			userService.saveUser(new User(null, "clement", "fasquel", "clement.fasquel@eurotutu.com", new ArrayList<>()));
+			userService.saveUser(new User(null, "weamix", "maxime", "maxime.vitse@decathlon.com", new HashSet<>()));
+			userService.saveUser(new User(null, "alebas", "lebas", "axel.lebas@decathlon.com", new HashSet<>()));
+			userService.saveUser(new User(null, "clement", "fasquel", "clement.fasquel@eurotutu.com", new HashSet<>()));
 
-			userService.addRoleToUser("clement", USER);
-			userService.addRoleToUser("weamix", ADMIN);
-			userService.addRoleToUser("alebas", ADMIN);
+			userService.addRoleToUser("clement", RoleName.USER);
+			userService.addRoleToUser("weamix", RoleName.ADMIN);
+			userService.addRoleToUser("alebas", RoleName.ADMIN);
 
 			FormDto formFeedback1 = new FormDto("Feedback annuel");
 			formService.addForm(formFeedback1);
